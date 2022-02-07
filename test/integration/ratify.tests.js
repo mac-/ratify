4b5b94d8d8dad342325487b2edc235d0be4fb52f
Patrick Malouin <patrick.malouin@auth0.com>
2022-02-07 14:49:07 -0500

    fix: make new test async

--- test/integration/ratify.tests.js
+++ test/integration/ratify.tests.js
@@ -103,23 +103,20 @@ describe('ratify plugin tests', function() {
 		assert(res.statusCode === 200);
 	});
 
-	it('should generate correct models for deeply nested schema', function(done) {
-		server.inject('/api-docs/clients', function (res) {
-			assert(res.statusCode === 200);
+	it('should generate correct models for deeply nested schema', async function() {
+		const res = await server.inject('/api-docs/clients')
+		assert(res.statusCode === 200);
 
-			var parsedPayload = JSON.parse(res.payload)
+		const parsedPayload = JSON.parse(res.payload)
 
-			var nestedRef = parsedPayload.models.get_clients_by_clientId_response.properties.nestedObject.$ref;
-			assert(nestedRef === 'get_clients_by_clientId_response_nestedObject');
-			assert(parsedPayload.models[nestedRef].type === 'object');
+		const nestedRef = parsedPayload.models.get_clients_by_clientId_response.properties.nestedObject.$ref;
+		assert(nestedRef === 'get_clients_by_clientId_response_nestedObject');
+		assert(parsedPayload.models[nestedRef].type === 'object');
 
-			var nestedRef2 = parsedPayload.models[nestedRef].properties.nestedObject2.$ref;
-			assert(nestedRef2 === 'get_clients_by_clientId_response_nestedObject2');
-			assert(parsedPayload.models[nestedRef2].properties.id.description === 'sample id field');
-			assert(parsedPayload.models[nestedRef2].properties.id.defaultValue === '123');
-
-			done();
-		});
+		const nestedRef2 = parsedPayload.models[nestedRef].properties.nestedObject2.$ref;
+		assert(nestedRef2 === 'get_clients_by_clientId_response_nestedObject2');
+		assert(parsedPayload.models[nestedRef2].properties.id.description === 'sample id field');
+		assert(parsedPayload.models[nestedRef2].properties.id.defaultValue === '123');
 	});
 
 	it('should validate path, headers, and response body against schema', async function() {
